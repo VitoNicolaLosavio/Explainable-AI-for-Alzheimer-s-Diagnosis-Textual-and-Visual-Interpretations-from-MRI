cbfbafe115b7db7e1a44508006e325f416f05aee
VitoNicolaLosavio <vito.nicola.losavio@gmail.com>
2024-09-12 14:44:31 +0200

    Developed transformer and finished project

---
+++ Models.py
@@ -0,0 +1,281 @@
+import os
+import math
+
+import torch
+import torch.nn as nn
+
+# this code is inspired by the code present on the
+# following github code:
+# https://github.com/LukeDitria/pytorch_tutorials/blob/main/section14_transformers/solutions/Pytorch5_Transformer_Image_Captioning.ipynb
+os.environ["TOKENIZERS_PARALLELISM"] = "false"
+
+torch.backends.cuda.matmul.allow_tf32 = True
+
+
+def extract_patches(image_tensor, patch_size=16):
+    # Get the dimensions of the image tensor
+    bs, c, h, w = image_tensor.size()
+
+    # Define the Unfold layer with appropriate parameters
+    unfold = torch.nn.Unfold(kernel_size=patch_size, stride=patch_size)
+
+    # Apply Unfold to the image tensor
+    unfolded = unfold(image_tensor)
+
+    # Reshape the unfolded tensor to match the desired output shape
+    # Output shape: BSxLxH, where L is the number of patches in each dimension
+    unfolded = unfolded.transpose(1, 2).reshape(bs, -1, c * patch_size * patch_size)
+
+    return unfolded
+
+
+# sinusoidal positional embeds
+class SinusoidalPosEmb(nn.Module):
+    def __init__(self, dim):
+        super().__init__()
+        self.dim = dim
+
+    def forward(self, x):
+        device = x.device
+        half_dim = self.dim // 2
+        emb = math.log(10000) / (half_dim - 1)
+        emb = torch.exp(torch.arange(half_dim, device=device) * -emb)
+        emb = x[:, None] * emb[None, :]
+        emb = torch.cat((emb.sin(), emb.cos()), dim=-1)
+        return emb
+
+
+# Define a module for attention blocks
+class AttentionBlock(nn.Module):
+    def __init__(self, hidden_size=128, num_heads=4, masking=True):
+        super(AttentionBlock, self).__init__()
+        self.masking = masking
+
+        # Multi-head attention mechanism
+        self.multihead_attn = nn.MultiheadAttention(hidden_size,
+                                                    num_heads=num_heads,
+                                                    batch_first=True,
+                                                    dropout=0.0)
+
+    def forward(self, x_in, kv_in, key_mask=None):
+        # Apply causal masking if enabled
+        if self.masking:
+            bs, l, h = x_in.shape
+            mask = torch.triu(torch.ones(l, l, device=x_in.device), 1).bool()
+        else:
+            mask = None
+
+        # Perform multi-head attention operation
+        return self.multihead_attn(x_in, kv_in, kv_in, attn_mask=mask,
+                                   key_padding_mask=key_mask)[0]
+
+
+# Define a module for a transformer block with self-attention
+# and optional causal masking
+class TransformerBlock(nn.Module):
+    def __init__(self, hidden_size=128, num_heads=4, decoder=False, masking=True):
+        super(TransformerBlock, self).__init__()
+        self.decoder = decoder
+
+        # Layer normalization for the input
+        self.norm1 = nn.LayerNorm(hidden_size)
+        # Self-attention mechanism
+        self.attn1 = AttentionBlock(hidden_size=hidden_size, num_heads=num_heads,
+                                    masking=masking)
+
+        # Layer normalization for the output of the first attention layer
+        if self.decoder:
+            self.norm2 = nn.LayerNorm(hidden_size)
+            # Self-attention mechanism for the decoder with no masking
+            self.attn2 = AttentionBlock(hidden_size=hidden_size,
+                                        num_heads=num_heads, masking=False)
+
+        # Layer normalization for the output before the MLP
+        self.norm_mlp = nn.LayerNorm(hidden_size)
+        # Multi-layer perceptron (MLP)
+        self.mlp = nn.Sequential(nn.Linear(hidden_size, hidden_size * 4),
+                                 nn.ELU(),
+                                 nn.Linear(hidden_size * 4, hidden_size))
+
+    def forward(self, x, input_key_mask=None, cross_key_mask=None, kv_cross=None):
+        # Perform self-attention operation
+        x = self.attn1(x, x, key_mask=input_key_mask) + x
+        x = self.norm1(x)
+
+        # If decoder, perform additional cross-attention layer
+        if self.decoder:
+            x = self.attn2(x, kv_cross, key_mask=cross_key_mask) + x
+            x = self.norm2(x)
+
+        # Apply MLP and layer normalization
+        x = self.mlp(x) + x
+        return self.norm_mlp(x)
+
+
+# Define a decoder module for the Transformer architecture
+class Decoder(nn.Module):
+    def __init__(self, num_emb, hidden_size=128, num_layers=3, num_heads=4):
+        super(Decoder, self).__init__()
+
+        # Create an embedding layer for tokens
+        self.embedding = nn.Embedding(num_emb, hidden_size)
+        # Initialize the embedding weights
+        self.embedding.weight.data = 0.001 * self.embedding.weight.data
+
+        # Initialize sinusoidal positional embeddings
+        self.pos_emb = SinusoidalPosEmb(hidden_size)
+
+        # Create multiple transformer blocks as layers
+        self.blocks = nn.ModuleList([
+            TransformerBlock(hidden_size, num_heads,
+                             decoder=True) for _ in range(num_layers)
+        ])
+
+        # Define a linear layer for output prediction
+        self.fc_out = nn.Linear(hidden_size, num_emb)
+
+    def forward(self, input_seq, encoder_output, input_padding_mask=None,
+                encoder_padding_mask=None):
+        # Embed the input sequence
+        input_embs = self.embedding(input_seq)
+        bs, l, h = input_embs.shape
+
+        # Add positional embeddings to the input embeddings
+        seq_indx = torch.arange(l, device=input_seq.device)
+        pos_emb = self.pos_emb(seq_indx).reshape(1, l, h).expand(bs, l, h)
+        embs = input_embs + pos_emb
+
+        # Pass the embeddings through each transformer block
+        for block in self.blocks:
+            embs = block(embs,
+                         input_key_mask=input_padding_mask,
+                         cross_key_mask=encoder_padding_mask,
+                         kv_cross=encoder_output)
+
+        return self.fc_out(embs)
+
+
+class VisionEncoder3D(nn.Module):
+
+    def __init__(self, image_size, channels_in, patch_size=16, hidden_size=128,
+                 num_layers=3, num_heads=4):
+        super(VisionEncoder3D, self).__init__()
+
+        # Define the 3D convolutional layers
+        self.conv3d_layer1 = nn.Conv3d(in_channels=channels_in, out_channels=32, kernel_size=3, stride=1, padding=1)
+        self.conv3d_layer2 = nn.Conv3d(in_channels=32, out_channels=64, kernel_size=3, stride=1, padding=1)
+
+        # Define the 2D convolutional layer
+        self.conv2d_layer = nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, stride=2, padding=1)
+
+        # da qui è il codice originale
+        self.patch_size = patch_size
+        self.fc_in = nn.Linear(image_size * patch_size * patch_size, hidden_size)
+
+        seq_length = (image_size // patch_size) * 4
+
+        self.pos_embedding = nn.Parameter(torch.empty(1, seq_length,
+                                                      hidden_size).normal_(std=0.02))
+
+        # Create multiple transformer blocks as layers
+        self.blocks = nn.ModuleList([
+            TransformerBlock(hidden_size, num_heads,
+                             decoder=False, masking=False) for _ in range(num_layers)
+        ])
+
+    def forward(self, image):
+        # Apply 3D convolutions
+        x = self.conv3d_layer1(image)
+        x = nn.ReLU()(x)
+        x = self.conv3d_layer2(x)
+        x = nn.ReLU()(x)
+        x = nn.AvgPool3d(kernel_size=(x.size(2), 1, 1))(x)
+        x = x.squeeze(2)
+
+        # Apply the 2D convolution
+        x = self.conv2d_layer(x)
+        x = nn.ReLU()(x)
+
+        # Original code
+        patch_seq = extract_patches(x, patch_size=self.patch_size)
+
+        patch_emb = self.fc_in(patch_seq)
+
+        # Add a unique embedding to each token embedding
+        embs = patch_emb + self.pos_embedding
+
+        # Pass the embeddings through each transformer block
+        for block in self.blocks:
+            embs = block(embs)
+
+        return embs
+
+
+# Define an Vision Encoder-Decoder module for the Transformer architecture
+class VisionEncoderDecoder(nn.Module):
+    def __init__(self, image_size, channels_in, num_emb, patch_size=16,
+                 hidden_size=128, num_layers=(3, 3), num_heads=4):
+        super(VisionEncoderDecoder, self).__init__()
+
+        # Create an encoder and decoder with specified parameters
+        self.encoder = VisionEncoder3D(
+            image_size=image_size,
+            channels_in=channels_in,
+            patch_size=patch_size,
+            hidden_size=hidden_size,
+            num_layers=num_layers[0],
+            num_heads=num_heads
+        )
+
+        self.decoder = Decoder(
+            num_emb=num_emb,
+            hidden_size=hidden_size,
+            num_layers=num_layers[1],
+            num_heads=num_heads
+        )
+
+    def forward(self, input_image, target_seq, padding_mask):
+        # Generate padding masks for the target sequence
+        bool_padding_mask = padding_mask == 0
+
+        # Encode the input sequence
+        encoded_seq = self.encoder(image=input_image)
+
+        # Decode the target sequence using the encoded sequence
+        decoded_seq = self.decoder(input_seq=target_seq,
+                                   encoder_output=encoded_seq,
+                                   input_padding_mask=bool_padding_mask)
+        return decoded_seq
+
+
+class TokenDrop(nn.Module):
+    """For a batch of tokens indices, randomly replace a non-specical token.
+
+    Args:
+        prob (float): probability of dropping a token
+        blank_token (int): index for the blank token
+        num_special (int): Number of special tokens, assumed to be at the start of the vocab
+    """
+
+    def __init__(self, prob=0.1, blank_token=1, eos_token=102):
+        self.prob = prob
+        self.eos_token = eos_token
+        self.blank_token = blank_token
+
+    def __call__(self, sample):
+        # Randomly sample a bernoulli distribution with p=prob
+        # to create a mask where 1 means we will replace that token
+        mask = torch.bernoulli(self.prob * torch.ones_like(sample)).long()
+
+        # only replace if the token is not the eos token
+        can_drop = (~(sample == self.eos_token)).long()
+        mask = mask * can_drop
+
+        # Do not replace the sos tokens
+        mask[:, 0] = torch.zeros_like(mask[:, 0]).long()
+
+        replace_with = (self.blank_token * torch.ones_like(sample)).long()
+
+        sample_out = (1 - mask) * sample + mask * replace_with
+
+        return sample_out
--- Utility.py
+++ Utility.py
@@ -1,4 +1,5 @@
-import os, warnings, torch, cv2
+import os, random, warnings, torch, cv2, textwrap
+
 import numpy as np
 import torch
 from monai.transforms import (
@@ -18,6 +19,13 @@ from monai.transforms import (
     Spacingd,
     SpatialPadd
 )
+
+from sys import platform
+import pandas as pd
+from sklearn.preprocessing import MinMaxScaler
+from matplotlib import pyplot as plt
+
+from alzheimer_disease.src.helpers.utils import get_device
 from alzheimer_disease.src.models.gradcam import GradCAM
 from alzheimer_disease.src.modules.preprocessing import get_transformations
 
@@ -29,26 +37,25 @@ def get_gradcam(
         threshold
 ):
     """
-	Computes Gradient-weighted Class Activation Mapping (Grad-CAM), and
-	returns a 3D segmentation mask over the `image` brain shape and above a fixed `threshold`.
-	Args:
-		example (dict): a testing set example.
-		model (torch.nn.Module): the model to be loaded.
-		saved_path (str): folder path where to find the model dump.
-		threshold (int | float): heat threshold above which draw the mask.
-		plot_results (bool): whether to plot the visual results.
-		alpha (int): transparency channel. Between 0 and 255.
-	Returns:
-		image (numpy.ndarray): the input 3D image.
-		mask (numpy.ndarray): the related computed 3D segmentation mask.
-		pred (int): model prediction.
-	"""
+    Computes Gradient-weighted Class Activation Mapping (Grad-CAM), and
+    returns a 3D segmentation mask over the `image` brain shape and above a fixed `threshold`.
+    Args:
+        example (dict): a testing set example.
+        model (torch.nn.Module): the model to be loaded.
+        saved_path (str): folder path where to find the model dump.
+        threshold (int | float): heat threshold above which draw the mask.
+        plot_results (bool): whether to plot the visual results.
+        alpha (int): transparency channel. Between 0 and 255.
+    Returns:
+        image (numpy.ndarray): the input 3D image.
+        mask (numpy.ndarray): the related computed 3D segmentation mask.
+        pred (int): model prediction.
+    """
     warnings.filterwarnings('ignore')
     try:
-        device = 'cpu'
+        device = get_device()
         model.load_state_dict(
-            torch.load(os.path.join(saved_path, model.name + '_best.pth'),
-                       map_location=torch.device(device))
+            torch.load(saved_path + model.name + '_best.pth')
         )
         model.to(device)
         _, eval_transform = get_transformations(model.in_size)
@@ -69,6 +76,7 @@ def get_gradcam(
         mask = _get_heatmap_mask(image, heatmap, threshold)
         return image, mask, pred, label, heatmap
     except OSError as e:
+        print(e)
         print('\n' + ''.join(['> ' for i in range(30)]))
         print('\nERROR: model dump for\033[95m ' + model.name + '\033[0m not found.\n')
         print(''.join(['> ' for i in range(30)]) + '\n')
@@ -76,15 +84,15 @@ def get_gradcam(
 
 def _get_heatmap_mask(image, heatmap, threshold):
     """
-	Computes a 3D segmentation mask over the `image` brain shape,
-	according to Grad-CAM `heatmap` and above a fixed `threshold`.
-	Args:
-		image (numpy.ndarray): the input 3D image.
-		heatmap (numpy.ndarray): the Grad-CAM 3D heatmap.
-		threshold (int): heat threshold above which draw the mask.
-	Returns:
-		mask (numpy.ndarray): the computed 3D segmentation mask.
-	"""
+    Computes a 3D segmentation mask over the `image` brain shape,
+    according to Grad-CAM `heatmap` and above a fixed `threshold`.
+    Args:
+        image (numpy.ndarray): the input 3D image.
+        heatmap (numpy.ndarray): the Grad-CAM 3D heatmap.
+        threshold (int): heat threshold above which draw the mask.
+    Returns:
+        mask (numpy.ndarray): the computed 3D segmentation mask.
+    """
     bg_mask = np.zeros(image.shape, dtype='uint8')
     heatmap_mask = np.zeros(image.shape, dtype='uint8')
     for z in range(image.shape[2]):
@@ -115,13 +123,13 @@ def _get_heatmap_mask(image, heatmap, threshold):
 
 def get_transformations_transformer(size):
     """
-	Get data transformation pipelines.
-	Args:
-		size (int): size for the input image. Final input shape will be (`size`, `size`, `size`).
-	Returns:
-		train_transform (monai.transforms.Compose): pipeline for the training input data.
-		eval_transform (monai.transforms.Compose): pipeline for the evaluation/testing input data.
-	"""
+    Get data transformation pipelines.
+    Args:
+        size (int): size for the input image. Final input shape will be (`size`, `size`, `size`).
+    Returns:
+        train_transform (monai.transforms.Compose): pipeline for the training input data.
+        eval_transform (monai.transforms.Compose): pipeline for the evaluation/testing input data.
+    """
     train_transform = Compose([
         LoadImaged(keys='image'),
         EnsureChannelFirstd(keys='image'),
@@ -164,3 +172,229 @@ def get_transformations_transformer(size):
         RandShiftIntensityd(keys='image', offsets=0.1, prob=1.0)
     ])
     return train_transform
+
+
+# I started with the train test split of colleague and adapt to my task
+def train_test_splitting(
+        data_folder,
+        meta_folder,
+        explanation_folder,
+        channels,
+        features,
+        train_ratio=.8,
+        multiclass=False,
+        verbose=True
+):
+    """
+    Splitting train/eval/test.
+    Args:
+        data_folder (str): path of the folder containing images.
+        meta_folder (str): path of the folder containing csv files.
+        explanation_folder (str): path of the folder containing csv files of the explanation.
+        channels (list): image channels to select (values `T1w`, `T2w` or both).
+        features (list): features set to select.
+        train_ratio (float): ratio of the training set, value between 0 and 1.
+        multiclass (bool): `False` for binary classification, `True` for ternary classification.
+        verbose (bool): whether or not print information.
+    Returns:
+        train_data (list): the training data ready to feed monai.data.Dataset
+        eval_data (list): the evaluation data ready to feed monai.data.Dataset
+        test_data (list): the testing data ready to feed monai.data.Dataset.
+        (see https://docs.monai.io/en/latest/data.html#monai.data.Dataset).
+    """
+    scaler = MinMaxScaler()
+    df = pd.read_csv(os.path.join(meta_folder, 'data_num.csv'))
+    df1 = df[(df['weight'] != .0) & (df['height'] != .0)]
+    df['bmi'] = round(df1['weight'] / (df1['height'] * df1['height']), 0)
+    df['bmi'] = df['bmi'].fillna(.0)
+    sessions = [s.split('_')[0] for s in os.listdir(data_folder) if os.path.isdir(os.path.join(data_folder, s))]
+    subjects = list(set(sessions))
+
+    # uploading of the dataset
+    explanation = pd.read_csv(explanation_folder + 'explaination.csv', sep=';')
+
+    # applying splitting on subjects to prevent data leakage
+    random.shuffle(subjects)
+    split_train = int(len(subjects) * train_ratio)
+    train_subjects, test_subjects = subjects[:split_train], subjects[split_train:]
+    split_eval = int(len(train_subjects) * .8)
+    eval_subjects = train_subjects[split_eval:]
+    train_subjects = train_subjects[:split_eval]
+
+    # applying multiclass label correction and splitting
+    if multiclass:
+        train_subjects, eval_subjects, test_subjects = [], [], []
+        df.loc[df['cdr'] == .0, 'final_dx'] = .0
+        df.loc[df['cdr'] == .5, 'final_dx'] = 1.
+        df.loc[(df['cdr'] != .0) & (df['cdr'] != .5), 'final_dx'] = 2.
+        m = np.min(np.unique(df['final_dx'].to_numpy(), return_counts=True)[1])
+        df = pd.concat([
+            df[df['final_dx'] == .0].sample(m),
+            df[df['final_dx'] == 1.].sample(m),
+            df[df['final_dx'] == 2.].sample(m)
+        ], ignore_index=True)
+        n_test = m - int(m * train_ratio)
+        n_eval = m - n_test - int(m * train_ratio * train_ratio)
+        for i in range(3):
+            sub = list(set(df[df['final_dx'] == float(i)]['subject_id'].to_numpy()))
+            random.shuffle(sub)
+            counter = 0
+            for j in range(len(sub)):
+                counter += len(df[df['subject_id'] == sub[j]])
+                if counter <= n_test:
+                    test_subjects.append(sub[j])
+                elif counter > n_test and counter <= (n_test + n_eval):
+                    eval_subjects.append(sub[j])
+                else:
+                    train_subjects.append(sub[j])
+
+    # loading sessions paths
+    X_train = df[df['subject_id'].isin(train_subjects)]
+    X_eval = df[df['subject_id'].isin(eval_subjects)]
+    X_test = df[df['subject_id'].isin(test_subjects)]
+    train_sessions = [os.path.join(data_folder, s) for s in X_train['session_id'].values]
+    eval_sessions = [os.path.join(data_folder, s) for s in X_eval['session_id'].values]
+    test_sessions = [os.path.join(data_folder, s) for s in X_test['session_id'].values]
+
+    # loading explanation of subjects
+    explanation_train = explanation[explanation['subject_id'].isin(X_train['subject_id'].values)]
+    explanation_eval = explanation[explanation['subject_id'].isin(X_eval['subject_id'].values)]
+    explanation_test = explanation[explanation['subject_id'].isin(X_test['subject_id'].values)]
+
+    # scaling numerical data in range [0,1]
+    X_train.loc[:, features] = scaler.fit_transform(X_train[features])
+    X_eval.loc[:, features] = scaler.fit_transform(X_eval[features])
+    X_test.loc[:, features] = scaler.fit_transform(X_test[features])
+
+    if platform == 'win32':
+        train_data = [dict({
+            'image': sorted([os.path.join(s, i) for i in os.listdir(s) if any(c in i for c in channels)]),
+            'data': X_train[X_train['session_id'] == s.split('\\')[-1]][features].values[0],
+            'label': df[df['session_id'] == s.split('\\')[-1]]['final_dx'].values[0],
+            'explanation':
+                explanation_train[explanation_train['session_id'] == s.split('\\')[-1]]['explaination'].values[0],
+            'session_id': s.split('\\')[-1]
+        }) for s in train_sessions]
+
+        eval_data = [dict({
+            'image': sorted([os.path.join(s, i) for i in os.listdir(s) if any(c in i for c in channels)]),
+            'data': X_eval[X_eval['session_id'] == s.split('\\')[-1]][features].values[0],
+            'label': df[df['session_id'] == s.split('\\')[-1]]['final_dx'].values[0],
+            'explanation': explanation_eval[explanation_eval['session_id'] == s.split('\\')[-1]]['explaination'].values[
+                0],
+            'session_id': s.split('\\')[-1]
+        }) for s in eval_sessions]
+
+        test_data = [dict({
+            'image': sorted([os.path.join(s, i) for i in os.listdir(s) if any(c in i for c in channels)]),
+            'data': X_test[X_test['session_id'] == s.split('\\')[-1]][features].values[0],
+            'label': df[df['session_id'] == s.split('\\')[-1]]['final_dx'].values[0],
+            'explanation': explanation_test[explanation_test['session_id'] == s.split('\\')[-1]]['explaination'].values[
+                0],
+            'session_id': s.split('\\')[-1]
+        }) for s in test_sessions]
+
+    else:
+        # arranging data in dictionaries
+        # I will also take the reference session of the explanation and the image
+        train_data = [dict({
+            'image': sorted([os.path.join(s, i) for i in os.listdir(s) if any(c in i for c in channels)]),
+            'data': X_train[X_train['session_id'] == s.split('/')[-1]][features].values[0],
+            'label': df[df['session_id'] == s.split('/')[-1]]['final_dx'].values[0],
+            'explanation':
+                explanation_train[explanation_train['session_id'] == s.split('/')[-1]]['explaination'].values[0],
+            'session_id': s.split('/')[-1]
+        }) for s in train_sessions]
+        eval_data = [dict({
+            'image': sorted([os.path.join(s, i) for i in os.listdir(s) if any(c in i for c in channels)]),
+            'data': X_eval[X_eval['session_id'] == s.split('/')[-1]][features].values[0],
+            'label': df[df['session_id'] == s.split('/')[-1]]['final_dx'].values[0],
+            'explanation': explanation_eval[explanation_eval['session_id'] == s.split('/')[-1]]['explaination'].values[
+                0],
+            'session_id': s.split('/')[-1]
+        }) for s in eval_sessions]
+        test_data = [dict({
+            'image': sorted([os.path.join(s, i) for i in os.listdir(s) if any(c in i for c in channels)]),
+            'data': X_test[X_test['session_id'] == s.split('/')[-1]][features].values[0],
+            'label': df[df['session_id'] == s.split('/')[-1]]['final_dx'].values[0],
+            'explanation': explanation_test[explanation_test['session_id'] == s.split('/')[-1]]['explaination'].values[
+                0],
+            'session_id': s.split('/')[-1]
+        }) for s in test_sessions]
+
+    # print data splitting information
+    if verbose:
+        print(''.join(['> ' for _ in range(40)]))
+        print(f'\n{"":<20}{"TRAINING":<20}{"EVALUATION":<20}{"TESTING":<20}\n')
+        print(''.join(['> ' for _ in range(40)]))
+        tsb1 = str(len(train_subjects)) + ' (' + str(
+            round((len(train_subjects) * 100 / len(df['subject_id'].unique())), 0)) + ' %)'
+        tsb2 = str(len(eval_subjects)) + ' (' + str(
+            round((len(eval_subjects) * 100 / len(df['subject_id'].unique())), 0)) + ' %)'
+        tsb3 = str(len(test_subjects)) + ' (' + str(
+            round((len(test_subjects) * 100 / len(df['subject_id'].unique())), 0)) + ' %)'
+        tss1 = str(len(train_sessions)) + ' (' + str(round((len(train_sessions) * 100 / len(df)), 2)) + ' %)'
+        tss2 = str(len(eval_sessions)) + ' (' + str(round((len(eval_sessions) * 100 / len(df)), 2)) + ' %)'
+        tss3 = str(len(test_sessions)) + ' (' + str(round((len(test_sessions) * 100 / len(df)), 2)) + ' %)'
+        print(f'\n{"subjects":<20}{tsb1:<20}{tsb2:<20}{tsb3:<20}\n')
+        print(f'{"sessions":<20}{tss1:<20}{tss2:<20}{tss3:<20}\n')
+
+    return train_data, eval_data, test_data
+
+
+def plot_grad_cam_explanation(image, label, pred, heatmap, mask, caption, alpha=128):
+    """
+    Plots model input image, Grad-CAM heatmap, segmentation mask and the explanation generated
+    Args:
+        image (numpy.ndarray): the input 3D image.
+        label (int): the input image label.
+        pred (int): model prediction for input image.
+        heatmap (numpy.ndarray): the Grad-CAM 3D heatmap.
+        mask (numpy.ndarray): the computed 3D segmentation mask.
+        caption (string): the explanation generated caption.
+        alpha (int): transparency channel. Between 0 and 255.
+    Returns:
+        None.
+    """
+    if alpha >= 0 and alpha <= 255:
+        heatmap_mask = np.zeros((image.shape[0], image.shape[1], image.shape[2], 4), dtype='uint8')
+        heatmap_mask[mask == 1] = [255, 0, 0, alpha]
+        image = image[:, :, int(image.shape[2] / 2)]
+        heatmap = heatmap[:, :, int(heatmap.shape[2] / 2)]
+        heatmap_mask = heatmap_mask[:, :, int(heatmap_mask.shape[2] / 2), :]
+        fig, axs = plt.subplots(1, 3, figsize=(18, 6))
+        norm_img = cv2.normalize(image, np.zeros((image.shape[1], image.shape[0])), 0, 1, cv2.NORM_MINMAX)
+        im_shows = [
+            axs[0].imshow(norm_img, cmap='gray', interpolation='bilinear', vmin=.0, vmax=1.),
+            axs[1].imshow(heatmap, cmap='jet', interpolation='bilinear', vmin=.0, vmax=1.),
+            axs[2].imshow(norm_img, cmap='gray', interpolation='bilinear', vmin=.0, vmax=1.)
+        ]
+        axs[2].imshow(heatmap_mask, interpolation='bilinear')
+        axs[0].set_title(
+            'Label=' + ('NON-AD' if label == 0 else 'AD') + ' | Prediction=' + ('NON-AD' if pred == 0 else 'AD'),
+            fontsize=16)
+        axs[1].set_title('Grad-CAM Heatmap', fontsize=16)
+        axs[2].set_title('Mask - Threshold ' + str(.8), fontsize=16)
+        for i, ax in enumerate(axs):
+            ax.axis('off')
+            fig.colorbar(im_shows[i], ax=ax, ticks=np.linspace(0, 1, 6))
+
+        # insert of caption generated
+        caption = "\n".join(textwrap.wrap(caption, width=230))
+        fig.text(0.5, -0.3, caption, ha='center',va='center')
+        fig.tight_layout()
+        fig.suptitle('Alzheimer classification and explaination', fontsize=30, y=1.1)
+
+        plt.show()
+    else:
+        print('\n' + ''.join(['> ' for i in range(30)]))
+        print('\nERROR: alpha channel \033[95m ' + alpha + '\033[0m out of range [0,255].\n')
+        print(''.join(['> ' for i in range(30)]) + '\n')
+
+
+def preprocessing_text(text: str):
+    text = text.lower()
+    text = text.replace('*','')
+    text = text.replace('\n',' ')
+    text = text.replace('\t',' ')
+    return text
